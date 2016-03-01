78956c86bc9eeea3de029391c28d54b3ce7e02b0
John Tibbetts <john.tibbetts@kinexis.com>
2016-03-01 15:32:22 -0800

    updates to conf and script

--- scripts/make_dirs_rdf.sh
+++ scripts/make_dirs_rdf.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 basedir=$1
 for dir in $(find "$basedir" -type d); do
 	container=$(basename "$dir")
@@ -5,34 +6,43 @@ for dir in $(find "$basedir" -type d); do
 		full_filename=$(basename "$f")
 		extension="${full_filename##*.}"
 		filename="${full_filename%.*}"
-		# echo Directory: $dir  Container: $container   Filename: $full_filename  #Extension: $extension
+		# echo Directory: $dir  Container: $container   Full_filename: $full_filename  #Extension: $extension
 
 		# if file is already in the correct form we're done with this dir
 		try_file=${PWD}/$dir/$container.$extension
 		if [[ $extension =~ (jsonld|ttl|rdf|html) ]]; then
 			if [ -f "$try_file" ]
 			then
-				: # echo $try_file ALREADY CORRECT
+				echo $try_file ALREADY CORRECT
+				continue
 			fi
-		else
-			# create a new file
-			if [[ $extension =~ (json|jsonld) ]]; then
-				# Uncomment following if downcasing filename is desired
-				# container=$(echo "$container"|tr '[A-Z]' '[a-z]')
+		fi
 
-				source_filename=${PWD}/$dir/$filename.$extension
-				if [[ $filename == $container ]]; then
-					target_filename=${PWD}/$dir/$container.jsonld
-					cp ${PWD}/$dir/$full_filename $target_filename
-				# normalize Greg name
-				elif [[ $filename == "JSON_LD_CONTEXT" ]]; then
-					target_filename=${PWD}/$dir/$container.jsonld
-					cp ${PWD}/$dir/$full_filename  $target_filename
-				fi
-				echo "$source_filename COPIED TO $target_filename "
-			else
-				echo ${PWD}/$dir/$full_filename UN-HANDLED
+		source_filename=${PWD}/$dir/$filename.$extension
+		# create a new file
+		if [[ $extension =~ (json|jsonld) ]]; then
+			# Uncomment following if downcasing filename is desired
+			# container=$(echo "$container"|tr '[A-Z]' '[a-z]')
+
+			if [[ $filename == $container ]]; then
+				target_filename=${PWD}/$dir/_content_.jsonld
+				cp ${PWD}/$dir/$full_filename $target_filename
+			# normalize Greg name
+			elif [[ $filename == "JSON_LD_CONTEXT" ]]; then
+				target_filename=${PWD}/$dir/_content_.jsonld
+				cp ${PWD}/$dir/$full_filename  $target_filename
 			fi
+			echo "$source_filename COPIED TO $target_filename "
+		elif [[ $full_filename == 'TURTLE.ttl' ]]; then
+			target_filename=${PWD}/$dir/_content_.ttl
+			cp ${PWD}/$dir/$full_filename  $target_filename
+			echo "$source_filename COPIED TO $target_filename "
+		elif [[ full_filename == 'index.html' ]]; then
+			target_filename=${PWD}/$dir/_content_.html
+			cp ${PWD}/$dir/$full_filename  $target_filename
+			echo "$source_filename COPIED TO $target_filename "
+		else
+			echo ${PWD}/$dir/$full_filename UN-HANDLED
 		fi
 	done
 done
